74d23e4e4fc56acb117573fb7ed451d0d6792bff
rajukumar <rajupraaa1234@gmail.com>
2021-08-15 01:47:44 +0530

    change Insert and update UI

--- lib/book/InsertBookScreen.dart
+++ lib/book/InsertBookScreen.dart
@@ -40,49 +40,54 @@ class _InsertBookScreenState extends State<InsertBookScreen> {
       appBar: AppBar(
         title: Text(appBarText)
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Form(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
-                child: TextFormField(
-                  controller: idController,
-                  decoration: _decorate("Enter book id", "Book Id")
+        body : Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(left: 0, top:60, right: 0, bottom:0),
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: idController,
+                      decoration: _decorate("Enter book id", "Book Id")
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
-                child: TextFormField(
-                  controller: nameController,
-                  decoration: _decorate("Enter book name", "Book Name")
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                      controller: nameController,
+                      decoration: _decorate("Enter book name", "Book Name")
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
-                child: TextFormField(
-                  controller: authorController,
-                  decoration: _decorate("Enter Author Name", "Author Name")
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                      controller: authorController,
+                      decoration: _decorate("Enter Author Name", "Author Name")
+                    // ),
+                  ),
                 ),
-              ),
-              TextFormField(
-                controller: priceController,
-                decoration: _decorate("Enter book price", "Price")
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  _insertOrUpdateBook();
-                },
-                child: Text(buttonName),
-              )
-            ],
-          )
-        ),
-      ),
-    );
-  }
-
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                      controller: priceController,
+                      decoration: _decorate("Enter book price", "Price")
+                  ),
+                ),
+                Container(
+                    height: 50,
+                    padding: EdgeInsets.fromLTRB(10, 15, 10, 0),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.blue,
+                      child: Text(buttonName),
+                      onPressed: () {
+                        _insertOrUpdateBook();
+                      },
+                    )),
+              ],
+            )));
+      }
   InputDecoration _decorate(String hintText, String labelText) {
     return InputDecoration(
       hintText: hintText,
